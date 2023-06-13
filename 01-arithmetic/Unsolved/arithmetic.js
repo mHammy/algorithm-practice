0aba3ea9dcb505f0cc8a4db1c02c08ca5c33bf07
mHammy <133612424+mHammy@users.noreply.github.com>
2023-06-13 14:08:58 -0400

    Initial commit with currently available activities

---
+++ 01-arithmetic/Unsolved/arithmetic.js
@@ -0,0 +1,24 @@
+// Fill in the body of each function with the code required
+var result = 0;
+
+var add = function(num1, num2) {
+    result = num1 + num2;
+    return result;
+};
+
+var subtract = function(num1, num2) {
+    result = num1 - num2;
+    return result;
+};
+
+var multiply = function(num1, num2) {
+    result = num1 * num2;
+    return result;
+};
+
+var divide = function(num1, num2) {
+    result = num1 / num2;
+    return result;
+};
+
+///Completed
